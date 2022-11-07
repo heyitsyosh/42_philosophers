1cdcf888ff391f1dbf97f7403ebf08aff0914724
Maria Yoshikawa <myoshika@c1r25s10.42tokyo.jp>
2022-11-07 22:18:21 +0900

    update

--- philosophers/philo_bonus/includes/philo_bonus.h
+++ philosophers/philo_bonus/includes/philo_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: myoshika <myoshika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 18:43:21 by myoshika          #+#    #+#             */
-/*   Updated: 2022/10/09 18:59:32 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/11/07 06:59:59 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <string.h> //memset
 # include <stdlib.h> //malloc, free, exit
 # include <unistd.h> //read, write, usleep, fork
+# include <stdbool.h>
 # include <sys/time.h> //gettimeofday
 # include <signal.h> //kill
 # include <pthread.h>
@@ -25,5 +26,30 @@
 # include <fcntl.h> //For O_* constants
 # include <sys/stat.h> //For mode constants
 # include <semaphore.h>
+
+# define FORK_MSG "has taken a fork"
+# define EAT_MSG "is eating"
+# define SLEEP_MSG "is sleeping"
+# define THINK_MSG "is thinking"
+# define DIE_MSG "died"
+
+typedef struct s_philo{
+	t_info		*i;
+	pthread_t	tid;
+	int			id;
+	int			left_fork;
+	int			right_fork;
+	size_t		meals_eaten;
+}	t_philo;
+
+typedef struct s_info{
+	int				num_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				max_meals;
+	long			start_time;
+	bool			overflow;
+}	t_info;
 
 #endif
---
+++ philosophers/philo_bonus/srcs/atoi_bonus.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atoi_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myoshika <myoshika@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/09 22:06:16 by myoshika          #+#    #+#             */
+/*   Updated: 2022/11/07 03:24:21 by myoshika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo_bonus.h"
+
+static int	is_whitespace(char c)
+{
+	return (c == 32 || (9 <= c && c <= 13));
+}
+
+static int	make_l(const char *str, size_t i, int sign, t_info *p)
+{
+	long	num;
+
+	num = 0;
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		if (sign == 1 && ((num > LONG_MAX / 10)
+				|| (num == LONG_MAX / 10 && str[i] - '0' > LONG_MAX % 10)))
+			p->overflow = true;
+		else if (sign == -1 && ((num < LONG_MIN / 10)
+				|| (num == LONG_MIN / 10 && str[i] - '0' > LONG_MIN % 10 * -1)))
+			p->overflow = true;
+		if (p->overflow)
+			break ;
+		num = (num * 10) + sign * (str[i] - '0');
+		i++;
+	}
+	return ((int)num);
+}
+
+int	philo_atoi(const char *str, t_info *p)
+{
+	size_t	i;
+	int		sign;
+
+	sign = 1;
+	i = 0;
+	while (is_whitespace(*(str + i)))
+		i++;
+	if ((*(str + i) == '+' || *(str + i) == '-') && *(str + i))
+	{
+		if (*(str + i) == '-')
+			sign = -1;
+		i++;
+	}
+	return (make_l(str, i, sign, p));
+}
