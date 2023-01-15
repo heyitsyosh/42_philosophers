22f3bcc5f0eaf1592d9f4137c72c44a6155ea264
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2023-01-15 10:55:57 +0900

    update

---
+++ philosophers/philo/srcs/time_utils.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 07:11:42 by myoshika          #+#    #+#             */
+/*   Updated: 2023/01/15 10:05:55 by myoshika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+#include <sys/time.h>
+
+long	time_in_ms(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec) * 1000 + (tv.tv_usec) / 1000);
+}
+
+long	time_in_usec(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_usec);
+}
+
+long	timestamp_in_ms(t_philo *philo)
+{
+	return (time_in_ms() - philo->start_time_ms);
+}
