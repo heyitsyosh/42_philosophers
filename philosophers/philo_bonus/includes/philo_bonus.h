f3e5a11fcf99ec0faa53ad28cd634ec08a3570e6
maria <winterzaq1@gmail.com>
2023-01-21 23:15:17 +0900

    bonus update: added philosopher processes creation, struct initialization, recycled some philo mandatory files

--- philosophers/philo_bonus/includes/philo_bonus.h
+++ philosophers/philo_bonus/includes/philo_bonus.h
@@ -6,25 +6,15 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 18:43:21 by myoshika          #+#    #+#             */
-/*   Updated: 2023/01/11 19:45:55 by myoshika         ###   ########.fr       */
+/*   Updated: 2023/01/21 23:09:05 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
 
-# include <stdio.h> //printf
-# include <stdlib.h> //malloc, free, exit
-# include <unistd.h> //read, write, usleep, fork
-# include <stdbool.h>
-# include <sys/time.h> //gettimeofday
-# include <signal.h> //kill
 # include <pthread.h>
-# include <sys/types.h>
-# include <sys/wait.h> //waitpid
-# include <fcntl.h> //For O_* constants
-# include <sys/stat.h> //For mode constants
-# include <semaphore.h>
+# include <stdbool.h>
 
 # define FORK_MSG "has taken a fork"
 # define EAT_MSG "is eating"
@@ -32,23 +22,25 @@
 # define THINK_MSG "is thinking"
 # define DIE_MSG "died"
 
+typedef struct s_info{
+	int				num_of_philosophers;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	int				meals_to_eat;
+	//bool			should_exit;
+	bool			overflow;
+}	t_info;
+
 typedef struct s_philo{
 	t_info		*i;
-	pthread_t	tid;
 	int			id;
-	int			left_fork;
-	int			right_fork;
 	size_t		meals_eaten;
 }	t_philo;
 
-typedef struct s_info{
-	int				num_of_philosophers;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				max_meals;
-	long			start_time;
-	bool			overflow;
-}	t_info;
+bool	convert_input(int argc, char **argv, t_info *p);
+int		philo_atoi(const char *str, t_info *p);
+
+void	make_philos(t_info *info);
 
 #endif
--- philosophers/philo_bonus/srcs/main.c
+++ philosophers/philo_bonus/srcs/main.c
@@ -6,11 +6,12 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 02:14:48 by myoshika          #+#    #+#             */
-/*   Updated: 2023/01/10 07:31:28 by myoshika         ###   ########.fr       */
+/*   Updated: 2023/01/21 23:11:42 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo_bonus.h"
+#include <stdlib.h>
 
 int	main(int argc, char **argv)
 {
@@ -23,13 +24,9 @@ int	main(int argc, char **argv)
 		printf("invalid arguments\n");
 		return (0);
 	}
-	philos = make_forks_and_philos(&i);
+	philos = malloc(sizeof(t_philo) * i.num_of_philosophers);
 	if (!philos)
-	{
-		printf("malloc failure\n");
-		return (1);
-	}
-	if (!make_philos(philos, &i))
-		return (1);
-	deinitialize(i.num_of_philosophers, philos, &i);
+		exit (EXIT_SUCCESS);
+	make_forks(&i);
+	make_philos(&i);
 }
